aa76081bdd9cf65b377ae6bfd79a1af28da239bc
Roy Pereira <roymap@users.noreply.github.com>
2023-07-12 00:12:36 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -235,6 +235,7 @@ export interface IIntegrationSupport {
 	list_application_id?: boolean;
 	list_candidate_id?: boolean;
 	list_deal_id?: boolean;
+	list_job_id?: boolean;
 	list_order?: boolean;
 	list_query?: boolean;
 	list_limit?: boolean;
