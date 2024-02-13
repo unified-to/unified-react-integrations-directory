6bb7a7cef06877e2c2110753a7e21f7b0d9d6ed2
Roy Pereira <roymap@users.noreply.github.com>
2024-02-12 19:28:21 -0500

    Unified.mongoose.ts

--- src/models/Unified.mongoose.ts
+++ src/models/Unified.mongoose.ts
@@ -49,8 +49,8 @@ export const schemaConnection = {
 	workspace_id: { type: SchemaTypes.ObjectId, index: true, ref: 'Workspace' },
 	integration_type: { type: String, index: true }, // The integration type
 	external_xref: { type: String, index: true }, // customer's user ID
-	permissions: { type: [ String ], enum: [ 'auth_login', 'accounting_account_read', 'accounting_account_write', 'accounting_transaction_read', 'accounting_transaction_write', 'accounting_invoice_read', 'accounting_invoice_write', 'accounting_contact_read', 'accounting_contact_write', 'accounting_payment_read', 'accounting_payment_write', 'accounting_taxrate_read', 'accounting_taxrate_write', 'accounting_organization_read', 'accounting_item_read', 'ats_scorecard_read', 'ats_scorecard_write', 'ats_application_read', 'ats_application_write', 'ats_applicationstatus_read', 'ats_candidate_read', 'ats_candidate_write', 'ats_interview_read', 'ats_interview_write', 'ats_job_read', 'ats_job_write', 'ats_document_read', 'ats_document_write', 'crm_company_read', 'crm_company_write', 'crm_contact_read', 'crm_contact_write', 'crm_deal_read', 'crm_deal_write', 'crm_event_read', 'crm_event_write', 'crm_lead_read', 'crm_lead_write', 'crm_pipeline_read', 'crm_pipeline_write', 'martech_list_read', 'martech_list_write', 'martech_member_read', 'martech_member_write', 'ticketing_customer_read', 'ticketing_customer_write', 'ticketing_ticket_read', 'ticketing_ticket_write', 'ticketing_note_read', 'ticketing_note_write', 'hris_employee_read', 'hris_employee_write', 'hris_group_read', 'hris_group_write', 'uc_call_read', 'storage_file_read', 'storage_file_write', 'webhook' ] },
-	categories: { type: [ String ], enum: [ 'passthrough', 'hris', 'ats', 'auth', 'crm', 'enrich', 'martech', 'ticketing', 'uc', 'accounting', 'storage' ], index: true }, // The Integration categories that this connection supports
+	permissions: { type: [ String ], enum: [ 'auth_login', 'accounting_account_read', 'accounting_account_write', 'accounting_transaction_read', 'accounting_transaction_write', 'accounting_invoice_read', 'accounting_invoice_write', 'accounting_contact_read', 'accounting_contact_write', 'accounting_payment_read', 'accounting_payment_write', 'accounting_taxrate_read', 'accounting_taxrate_write', 'accounting_organization_read', 'accounting_payout_read', 'accounting_refund_read', 'commerce_item_read', 'commerce_item_write', 'commerce_collection_read', 'commerce_collection_write', 'commerce_inventory_read', 'commerce_inventory_write', 'commerce_location_read', 'commerce_location_write', 'ats_scorecard_read', 'ats_scorecard_write', 'ats_application_read', 'ats_application_write', 'ats_applicationstatus_read', 'ats_candidate_read', 'ats_candidate_write', 'ats_interview_read', 'ats_interview_write', 'ats_job_read', 'ats_job_write', 'ats_company_read', 'ats_document_read', 'ats_document_write', 'crm_company_read', 'crm_company_write', 'crm_contact_read', 'crm_contact_write', 'crm_deal_read', 'crm_deal_write', 'crm_event_read', 'crm_event_write', 'crm_lead_read', 'crm_lead_write', 'crm_pipeline_read', 'crm_pipeline_write', 'martech_list_read', 'martech_list_write', 'martech_member_read', 'martech_member_write', 'ticketing_customer_read', 'ticketing_customer_write', 'ticketing_ticket_read', 'ticketing_ticket_write', 'ticketing_note_read', 'ticketing_note_write', 'hris_employee_read', 'hris_employee_write', 'hris_group_read', 'hris_group_write', 'uc_call_read', 'storage_file_read', 'storage_file_write', 'webhook' ] },
+	categories: { type: [ String ], enum: [ 'passthrough', 'hris', 'ats', 'auth', 'crm', 'enrich', 'martech', 'ticketing', 'uc', 'accounting', 'storage', 'commerce' ], index: true }, // The Integration categories that this connection supports
 	auth: { type: schemaConnectionAuth },
 	is_paused: { type: Boolean }, // Whether this integration has exceed the monthly limit of the plan
 	auth_aws_arn: { type: String }, // the AWS ARN / secretID for the stored auth field
@@ -95,6 +95,20 @@ export const schemaIntegrationSupport = {
 	list_ticket_id: { type: Boolean },
 };
 
+export const schemaInvoice = {
+};
+
+export const schemaIssue = {
+	created_at: { type: String },
+	updated_at: { type: String },
+	title: { type: String },
+	status: { type: String, enum: [ 'ON_HOLD', 'NEW', 'IN_PROGRESS', 'VALIDATING', 'COMPLETE', 'REJECTED' ] },
+	url: { type: String },
+	workspace_id: { type: SchemaTypes.ObjectId, index: true, ref: 'Workspace' },
+	type: { type: String, enum: [ 'BUG', 'FEATURE_REQUEST', 'INQUIRY' ] },
+	resolution_time: { type: Number },
+};
+
 export const schemaNotification = {
 	workspace_id: { type: SchemaTypes.ObjectId, index: true, ref: 'Workspace' },
 	title: { type: String }, // The title of the notification
@@ -119,7 +133,7 @@ export const schemaWebhook = {
 	workspace_id: { type: SchemaTypes.ObjectId, index: true, ref: 'Workspace' },
 	connection_id: { type: SchemaTypes.ObjectId, index: true, ref: 'Integration' },
 	hook_url: { type: String }, // The URL of the webhook
-	object_type: { type: String, enum: [ 'accounting_account', 'accounting_transaction', 'accounting_contact', 'accounting_invoice', 'accounting_payment', 'accounting_taxrate', 'accounting_organization', 'accounting_item', 'ats_application', 'ats_applicationstatus', 'ats_candidate', 'ats_document', 'ats_interview', 'ats_job', 'ats_scorecard', 'crm_company', 'crm_contact', 'crm_deal', 'crm_event', 'crm_lead', 'crm_pipeline', 'hris_employee', 'hris_group', 'martech_list', 'martech_member', 'passthrough', 'ticketing_note', 'ticketing_ticket', 'ticketing_customer', 'uc_contact', 'uc_call', 'enrich_person', 'enrich_company', 'storage_file' ] }, // The object to return (eg. CRM "contact")
+	object_type: { type: String, enum: [ 'accounting_account', 'accounting_transaction', 'accounting_contact', 'accounting_invoice', 'accounting_payment', 'accounting_taxrate', 'accounting_organization', 'accounting_payout', 'accounting_refund', 'commerce_item', 'commerce_collection', 'commerce_inventory', 'commerce_location', 'ats_application', 'ats_applicationstatus', 'ats_candidate', 'ats_document', 'ats_interview', 'ats_job', 'ats_scorecard', 'ats_company', 'crm_company', 'crm_contact', 'crm_deal', 'crm_event', 'crm_lead', 'crm_pipeline', 'hris_employee', 'hris_group', 'martech_list', 'martech_member', 'passthrough', 'ticketing_note', 'ticketing_ticket', 'ticketing_customer', 'uc_contact', 'uc_call', 'enrich_person', 'enrich_company', 'storage_file' ] }, // The object to return (eg. CRM "contact")
 	interval: { type: Number }, // The interval (in minutes) to check for updated/new objets.  Minimum is 5 minutes.  Interval is based off of 5-minute increments.
 	checked_at: { type: Date }, // The last date/time that a check was done on this object
 	integration_type: { type: String },
@@ -130,6 +144,7 @@ export const schemaWebhook = {
 	webhook_type: { type: String, enum: [ 'virtual', 'none', 'native' ] },
 	meta: { type: Object },
 	is_healthy: { type: Boolean },
+	page_max_limit: { type: Number },
 };
 
 export const schemaWebhookData = {
@@ -153,7 +168,7 @@ export const schemaWorkspaceIntegration = {
 	pem: { type: String }, // the PEM X.509 certificate in Base64 ASCII format
 	key: { type: String }, // the private KEY X.509 certificate in Base64 ASCII format
 	environment: { type: String, default: 'Production' }, // authentication environment
-	categories: { type: [ String ], enum: [ 'passthrough', 'hris', 'ats', 'auth', 'crm', 'enrich', 'martech', 'ticketing', 'uc', 'accounting', 'storage' ] },
+	categories: { type: [ String ], enum: [ 'passthrough', 'hris', 'ats', 'auth', 'crm', 'enrich', 'martech', 'ticketing', 'uc', 'accounting', 'storage', 'commerce' ] },
 };
 
 export const schemaWorkspace = {
