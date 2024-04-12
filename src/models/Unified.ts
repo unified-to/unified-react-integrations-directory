7b34cdbd034815759d1662af2d7d433125447fcc
Roy Pereira <roymap@users.noreply.github.com>
2024-04-12 14:49:07 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -145,28 +145,6 @@ export const IssueType = [
 
 export type TIssueType = typeof IssueType[number];
 
-export const WorkspaceEventType = [
-	'USER_CREATED',
-	'USER_DELETED',
-	'CONNECTION_HEALTHY',
-	'CONNECTION_UNHEALTHY',
-	'CONNECTION_CREATED',
-	'CONNECTION_UPDATED',
-	'CONNECTION_DELETED',
-	'CONNECTION_PAUSED',
-	'CONNECTION_UNPAUSED',
-	'INTEGRATION_ACTIVATED',
-	'INTEGRATION_DEACTIVATED',
-	'INTEGRATION_UPDATED',
-	'WORKSPACE_UPDATED',
-	'WORKSPACE_OVER_LIMIT',
-	'WORKSPACE_80PERCENT_LIMIT',
-	'WEBHOOK_CREATED',
-	'WEBHOOK_DELETED'
-] as const;
-
-export type TWorkspaceEventType = typeof WorkspaceEventType[number];
-
 export const ObjectType = [
 	'accounting_account',
 	'accounting_transaction',
@@ -254,6 +232,28 @@ export const WebhookEvent = [
 ] as const;
 
 export type TWebhookEvent = typeof WebhookEvent[number];
+
+export const WorkspaceEventType = [
+	'USER_CREATED',
+	'USER_DELETED',
+	'CONNECTION_HEALTHY',
+	'CONNECTION_UNHEALTHY',
+	'CONNECTION_CREATED',
+	'CONNECTION_UPDATED',
+	'CONNECTION_DELETED',
+	'CONNECTION_PAUSED',
+	'CONNECTION_UNPAUSED',
+	'INTEGRATION_ACTIVATED',
+	'INTEGRATION_DEACTIVATED',
+	'INTEGRATION_UPDATED',
+	'WORKSPACE_UPDATED',
+	'WORKSPACE_OVER_LIMIT',
+	'WORKSPACE_80PERCENT_LIMIT',
+	'WEBHOOK_CREATED',
+	'WEBHOOK_DELETED'
+] as const;
+
+export type TWorkspaceEventType = typeof WorkspaceEventType[number];
 
 export interface IApiCall {
 	readonly id?: string; // Unique identifier for this API call
