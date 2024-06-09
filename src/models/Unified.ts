1bd6dea715fc095771568b1a69c10dc595f48ec9
Roy Pereira <roymap@users.noreply.github.com>
2024-06-08 22:23:56 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -551,6 +551,7 @@ export interface IWorkspace {
 	domain?: string; // when set, users of the same domain will auto-join this workspace.  must not be gmail.com or other public domains
 	event_webhook_url?: string;
 	event_webhook_events?: ( TWorkspaceEventType )[];
+	custom_auth_domain?: string;
 }
 
 export interface IWorkspaceIntegration {
