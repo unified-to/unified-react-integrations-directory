ec78c66f10442c761d0756016a0ee990232dffb3
Roy Pereira <roymap@users.noreply.github.com>
2024-11-22 15:51:47 -0500

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -465,6 +465,7 @@ export interface IIntegrationSupport {
 	virtual_webhook_updated_gte?: TSupportInboundType;
 	virtual_webhook_limit?: IIntegrationSupport;
 	virtual_webhook_channel_id?: IIntegrationSupport;
+	virtual_webhook_parent_id?: IIntegrationSupport;
 }
 
 export interface IInvoice {
