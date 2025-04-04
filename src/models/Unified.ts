ed269bebe092a30d102c6d9b3c8ed97cdd2f99f4
Roy Pereira <roymap@users.noreply.github.com>
2025-04-04 09:47:50 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -682,6 +682,7 @@ export interface IWorkspace {
 	custom_auth_domain_valid?: boolean;
 	custom_api_domain?: string;
 	slow_fields_opt_in?: boolean;
+	notion_verification_token?: string;
 }
 
 export interface IWorkspaceIntegration {
