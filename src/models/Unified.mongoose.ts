fc8eae9b69a84aae9a515c22640c6a6aeb1e4dbe
Roy Pereira <roymap@users.noreply.github.com>
2025-04-24 09:51:30 -0400

    Unified.mongoose.ts

--- src/models/Unified.mongoose.ts
+++ src/models/Unified.mongoose.ts
@@ -30,6 +30,7 @@ export const schemaConnectionAuth = {
 	expires_in: { type: Number },
 	emails: { type: [ String ] },
 	name: { type: String },
+	user_id: { type: String },
 	app_id: { type: String },
 	client_id: { type: String },
 	client_secret: { type: String },
@@ -53,8 +54,8 @@ export const schemaConnection = {
 	integration_type: { type: String, index: true }, // The integration type
 	integration_name: { type: String },
 	external_xref: { type: String, index: true }, // customer's user ID
-	permissions: { type: [ String ], enum: [ 'auth_login', 'accounting_account_read', 'accounting_account_write', 'accounting_transaction_read', 'accounting_transaction_write', 'accounting_journal_read', 'accounting_journal_write', 'accounting_invoice_read', 'accounting_invoice_write', 'accounting_contact_read', 'accounting_contact_write', 'accounting_taxrate_read', 'accounting_taxrate_write', 'accounting_organization_read', 'accounting_order_read', 'accounting_order_write', 'payment_payment_read', 'payment_payment_write', 'payment_payout_read', 'payment_refund_read', 'payment_link_read', 'payment_link_write', 'payment_subscription_read', 'payment_subscription_write', 'commerce_item_read', 'commerce_item_write', 'commerce_collection_read', 'commerce_collection_write', 'commerce_inventory_read', 'commerce_inventory_write', 'commerce_location_read', 'commerce_location_write', 'ats_activity_read', 'ats_activity_write', 'ats_application_read', 'ats_application_write', 'ats_applicationstatus_read', 'ats_candidate_read', 'ats_candidate_write', 'ats_interview_read', 'ats_interview_write', 'ats_job_read', 'ats_job_write', 'ats_company_read', 'ats_document_read', 'ats_document_write', 'ats_scorecard_read', 'ats_scorecard_write', 'crm_company_read', 'crm_company_write', 'crm_contact_read', 'crm_contact_write', 'crm_deal_read', 'crm_deal_write', 'crm_event_read', 'crm_event_write', 'crm_lead_read', 'crm_lead_write', 'crm_pipeline_read', 'crm_pipeline_write', 'martech_list_read', 'martech_list_write', 'martech_member_read', 'martech_member_write', 'ticketing_customer_read', 'ticketing_customer_write', 'ticketing_ticket_read', 'ticketing_ticket_write', 'ticketing_note_read', 'ticketing_note_write', 'hris_employee_read', 'hris_employee_write', 'hris_group_read', 'hris_group_write', 'hris_payslip_read', 'hris_payslip_write', 'hris_timeoff_read', 'hris_timeoff_write', 'hris_company_read', 'hris_company_write', 'hris_location_read', 'hris_location_write', 'uc_call_read', 'uc_contact_read', 'uc_contact_write', 'storage_file_read', 'storage_file_write', 'webhook', 'genai_model_read', 'genai_prompt_read', 'genai_prompt_write', 'messaging_message_read', 'messaging_message_write', 'messaging_channel_read', 'kms_space_read', 'kms_space_write', 'kms_page_read', 'kms_page_write', 'kms_comment_read', 'kms_comment_write', 'task_project_read', 'task_project_write', 'task_task_read', 'task_task_write', 'task_comment_read', 'task_comment_write', 'scim_users_read', 'scim_users_write', 'scim_groups_read', 'scim_groups_write', 'lms_course_read', 'lms_course_write', 'lms_class_read', 'lms_class_write', 'lms_student_read', 'lms_student_write', 'lms_instructor_read', 'lms_instructor_write', 'repo_organization_read', 'repo_organization_write', 'repo_repository_read', 'repo_repository_write', 'repo_branch_read', 'repo_branch_write', 'repo_commit_read', 'repo_commit_write', 'repo_pullrequest_read', 'repo_pullrequest_write', 'metadata_metadata_read', 'metadata_metadata_write' ] },
-	categories: { type: [ String ], enum: [ 'passthrough', 'hris', 'ats', 'auth', 'crm', 'enrich', 'martech', 'ticketing', 'uc', 'accounting', 'storage', 'commerce', 'payment', 'genai', 'messaging', 'kms', 'task', 'scim', 'lms', 'repo', 'metadata' ], index: true }, // The Integration categories that this connection supports
+	permissions: { type: [ String ], enum: [ 'auth_login', 'accounting_account_read', 'accounting_account_write', 'accounting_transaction_read', 'accounting_transaction_write', 'accounting_journal_read', 'accounting_journal_write', 'accounting_invoice_read', 'accounting_invoice_write', 'accounting_contact_read', 'accounting_contact_write', 'accounting_taxrate_read', 'accounting_taxrate_write', 'accounting_organization_read', 'accounting_order_read', 'accounting_order_write', 'accounting_report_read', 'accounting_report_write', 'payment_payment_read', 'payment_payment_write', 'payment_payout_read', 'payment_refund_read', 'payment_link_read', 'payment_link_write', 'payment_subscription_read', 'payment_subscription_write', 'commerce_item_read', 'commerce_item_write', 'commerce_collection_read', 'commerce_collection_write', 'commerce_inventory_read', 'commerce_inventory_write', 'commerce_location_read', 'commerce_location_write', 'ats_activity_read', 'ats_activity_write', 'ats_application_read', 'ats_application_write', 'ats_applicationstatus_read', 'ats_candidate_read', 'ats_candidate_write', 'ats_interview_read', 'ats_interview_write', 'ats_job_read', 'ats_job_write', 'ats_company_read', 'ats_document_read', 'ats_document_write', 'ats_scorecard_read', 'ats_scorecard_write', 'crm_company_read', 'crm_company_write', 'crm_contact_read', 'crm_contact_write', 'crm_deal_read', 'crm_deal_write', 'crm_event_read', 'crm_event_write', 'crm_lead_read', 'crm_lead_write', 'crm_pipeline_read', 'crm_pipeline_write', 'martech_list_read', 'martech_list_write', 'martech_member_read', 'martech_member_write', 'ticketing_customer_read', 'ticketing_customer_write', 'ticketing_ticket_read', 'ticketing_ticket_write', 'ticketing_note_read', 'ticketing_note_write', 'hris_employee_read', 'hris_employee_write', 'hris_group_read', 'hris_group_write', 'hris_payslip_read', 'hris_payslip_write', 'hris_timeoff_read', 'hris_timeoff_write', 'hris_company_read', 'hris_company_write', 'hris_location_read', 'hris_location_write', 'hris_device_read', 'hris_device_write', 'uc_call_read', 'uc_contact_read', 'uc_contact_write', 'storage_file_read', 'storage_file_write', 'webhook', 'genai_model_read', 'genai_prompt_read', 'genai_prompt_write', 'messaging_message_read', 'messaging_message_write', 'messaging_channel_read', 'kms_space_read', 'kms_space_write', 'kms_page_read', 'kms_page_write', 'kms_comment_read', 'kms_comment_write', 'task_project_read', 'task_project_write', 'task_task_read', 'task_task_write', 'task_comment_read', 'task_comment_write', 'scim_users_read', 'scim_users_write', 'scim_groups_read', 'scim_groups_write', 'lms_course_read', 'lms_course_write', 'lms_class_read', 'lms_class_write', 'lms_student_read', 'lms_student_write', 'lms_instructor_read', 'lms_instructor_write', 'repo_organization_read', 'repo_organization_write', 'repo_repository_read', 'repo_repository_write', 'repo_branch_read', 'repo_branch_write', 'repo_commit_read', 'repo_commit_write', 'repo_pullrequest_read', 'repo_pullrequest_write', 'metadata_metadata_read', 'metadata_metadata_write', 'calendar_calendar_read', 'calendar_calendar_write', 'calendar_event_read', 'calendar_event_write', 'calendar_busy_read', 'calendar_busy_write', 'calendar_link_read', 'calendar_link_write', 'calendar_recording_read', 'calendar_recording_write', 'enrich_person_read', 'enrich_company_read' ] },
+	categories: { type: [ String ], enum: [ 'passthrough', 'hris', 'ats', 'auth', 'crm', 'enrich', 'martech', 'ticketing', 'uc', 'accounting', 'storage', 'commerce', 'payment', 'genai', 'messaging', 'kms', 'task', 'scim', 'lms', 'repo', 'metadata', 'calendar' ], index: true }, // The Integration categories that this connection supports
 	auth: { type: schemaConnectionAuth },
 	is_paused: { type: Boolean }, // Whether this integration has exceed the monthly limit of the plan
 	auth_aws_arn: { type: String }, // the AWS ARN / secretID for the stored auth field
@@ -116,6 +117,8 @@ export const schemaIntegrationSupport = {
 	list_class_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
 	list_repo_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
 	list_org_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
+	list_calendar_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
+	list_event_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
 	list_task_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
 	virtual_webhook_updated_gte: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
 	virtual_webhook_limit: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
@@ -130,6 +133,9 @@ export const schemaIntegrationSupport = {
 	native_webhook_parent_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
 	native_webhook_project_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
 	virtual_webhook_job_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
+	list_start_gte: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
+	list_end_le: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
+	list_lead_id: { type: String, enum: [ 'supported-required', 'supported', 'not-supported' ] },
 };
 
 export const schemaInvoice = {
@@ -158,7 +164,7 @@ export const schemaNotification = {
 	integration_type: { type: String },
 	integration_name: { type: String },
 	sent_at: { type: Date },
-	event: { type: String, enum: [ 'USER_CREATED', 'USER_DELETED', 'CONNECTION_HEALTHY', 'CONNECTION_UNHEALTHY', 'CONNECTION_CREATED', 'CONNECTION_UPDATED', 'CONNECTION_DELETED', 'CONNECTION_PAUSED', 'CONNECTION_UNPAUSED', 'INTEGRATION_ACTIVATED', 'INTEGRATION_DEACTIVATED', 'INTEGRATION_UPDATED', 'WORKSPACE_UPDATED', 'WORKSPACE_OVER_LIMIT', 'WORKSPACE_80PERCENT_LIMIT', 'WEBHOOK_CREATED', 'WEBHOOK_DELETED', 'WEBHOOK_UNHEALTHY' ] },
+	event: { type: String, enum: [ 'USER_CREATED', 'USER_DELETED', 'CONNECTION_HEALTHY', 'CONNECTION_UNHEALTHY', 'CONNECTION_CREATED', 'CONNECTION_UPDATED', 'CONNECTION_DELETED', 'CONNECTION_PAUSED', 'CONNECTION_UNPAUSED', 'INTEGRATION_ACTIVATED', 'INTEGRATION_DEACTIVATED', 'INTEGRATION_UPDATED', 'WORKSPACE_UPDATED', 'WORKSPACE_OVER_LIMIT', 'WORKSPACE_80PERCENT_LIMIT', 'WEBHOOK_CREATED', 'WEBHOOK_DELETED', 'WEBHOOK_UNHEALTHY', 'WEBHOOK_PAUSED', 'WEBHOOK_UNPAUSED' ] },
 };
 
 export const schemaUser = {
@@ -174,7 +180,7 @@ export const schemaWebhook = {
 	workspace_id: { type: SchemaTypes.ObjectId, index: true, ref: 'Workspace' },
 	connection_id: { type: SchemaTypes.ObjectId, index: true, ref: 'Integration' },
 	hook_url: { type: String }, // The URL of the webhook
-	object_type: { type: String, enum: [ 'accounting_account', 'accounting_transaction', 'accounting_journal', 'accounting_contact', 'accounting_invoice', 'accounting_taxrate', 'accounting_organization', 'accounting_order', 'payment_payment', 'payment_link', 'payment_payout', 'payment_refund', 'payment_subscription', 'commerce_item', 'commerce_collection', 'commerce_inventory', 'commerce_location', 'ats_activity', 'ats_application', 'ats_applicationstatus', 'ats_candidate', 'ats_document', 'ats_interview', 'ats_job', 'ats_scorecard', 'ats_company', 'crm_company', 'crm_contact', 'crm_deal', 'crm_event', 'crm_lead', 'crm_pipeline', 'hris_employee', 'hris_group', 'hris_payslip', 'hris_timeoff', 'hris_company', 'hris_location', 'martech_list', 'martech_member', 'passthrough', 'ticketing_note', 'ticketing_ticket', 'ticketing_customer', 'uc_contact', 'uc_call', 'enrich_person', 'enrich_company', 'storage_file', 'genai_model', 'genai_prompt', 'messaging_message', 'messaging_channel', 'kms_space', 'kms_page', 'kms_comment', 'task_project', 'task_task', 'task_comment', 'scim_users', 'scim_groups', 'lms_course', 'lms_class', 'lms_student', 'lms_instructor', 'scim_groups', 'repo_organization', 'repo_repository', 'repo_branch', 'repo_commit', 'repo_pullrequest', 'metadata_metadata' ] }, // The object to return (eg. CRM "contact")
+	object_type: { type: String, enum: [ 'accounting_account', 'accounting_transaction', 'accounting_journal', 'accounting_contact', 'accounting_invoice', 'accounting_taxrate', 'accounting_organization', 'accounting_order', 'accounting_report', 'payment_payment', 'payment_link', 'payment_payout', 'payment_refund', 'payment_subscription', 'commerce_item', 'commerce_collection', 'commerce_inventory', 'commerce_location', 'ats_activity', 'ats_application', 'ats_applicationstatus', 'ats_candidate', 'ats_document', 'ats_interview', 'ats_job', 'ats_scorecard', 'ats_company', 'crm_company', 'crm_contact', 'crm_deal', 'crm_event', 'crm_lead', 'crm_pipeline', 'hris_employee', 'hris_group', 'hris_payslip', 'hris_timeoff', 'hris_company', 'hris_location', 'hris_device', 'martech_list', 'martech_member', 'passthrough', 'ticketing_note', 'ticketing_ticket', 'ticketing_customer', 'uc_contact', 'uc_call', 'enrich_person', 'enrich_company', 'storage_file', 'genai_model', 'genai_prompt', 'messaging_message', 'messaging_channel', 'kms_space', 'kms_page', 'kms_comment', 'task_project', 'task_task', 'task_comment', 'scim_users', 'scim_groups', 'lms_course', 'lms_class', 'lms_student', 'lms_instructor', 'repo_organization', 'repo_repository', 'repo_branch', 'repo_commit', 'repo_pullrequest', 'metadata_metadata', 'calendar_calendar', 'calendar_event', 'calendar_busy', 'calendar_link', 'calendar_recording' ] }, // The object to return (eg. CRM "contact")
 	interval: { type: Number }, // The interval (in minutes) to check for updated/new objets.  Minimum is 5 minutes.  Interval is based off of 5-minute increments.
 	checked_at: { type: Date }, // The last date/time that a check was done on this object
 	integration_type: { type: String },
@@ -187,6 +193,10 @@ export const schemaWebhook = {
 	is_healthy: { type: Boolean },
 	page_max_limit: { type: Number },
 	filters: { type: Object },
+	db_type: { type: String, enum: [ 'mongodb', 'mysql', 'postgres', 'mssql', 'mariadb' ] },
+	db_url: { type: String },
+	db_name_prefix: { type: String },
+	is_paused: { type: Boolean },
 };
 
 export const schemaWebhookData = {
@@ -212,7 +222,7 @@ export const schemaWorkspaceIntegration = {
 	pem: { type: String }, // the PEM X.509 certificate in Base64 ASCII format
 	key: { type: String }, // the private KEY X.509 certificate in Base64 ASCII format
 	environment: { type: String, default: 'Production' }, // authentication environment
-	categories: { type: [ String ], enum: [ 'passthrough', 'hris', 'ats', 'auth', 'crm', 'enrich', 'martech', 'ticketing', 'uc', 'accounting', 'storage', 'commerce', 'payment', 'genai', 'messaging', 'kms', 'task', 'scim', 'lms', 'repo', 'metadata' ] },
+	categories: { type: [ String ], enum: [ 'passthrough', 'hris', 'ats', 'auth', 'crm', 'enrich', 'martech', 'ticketing', 'uc', 'accounting', 'storage', 'commerce', 'payment', 'genai', 'messaging', 'kms', 'task', 'scim', 'lms', 'repo', 'metadata', 'calendar' ] },
 	dev_api_key: { type: String },
 };
 
@@ -240,9 +250,11 @@ export const schemaWorkspace = {
 	stripe_canceling_at: { type: Date },
 	domain: { type: String }, // when set, users of the same domain will auto-join this workspace.  must not be gmail.com or other public domains
 	event_webhook_url: { type: String },
-	event_webhook_events: { type: [ String ], enum: [ 'USER_CREATED', 'USER_DELETED', 'CONNECTION_HEALTHY', 'CONNECTION_UNHEALTHY', 'CONNECTION_CREATED', 'CONNECTION_UPDATED', 'CONNECTION_DELETED', 'CONNECTION_PAUSED', 'CONNECTION_UNPAUSED', 'INTEGRATION_ACTIVATED', 'INTEGRATION_DEACTIVATED', 'INTEGRATION_UPDATED', 'WORKSPACE_UPDATED', 'WORKSPACE_OVER_LIMIT', 'WORKSPACE_80PERCENT_LIMIT', 'WEBHOOK_CREATED', 'WEBHOOK_DELETED', 'WEBHOOK_UNHEALTHY' ] },
+	event_webhook_events: { type: [ String ], enum: [ 'USER_CREATED', 'USER_DELETED', 'CONNECTION_HEALTHY', 'CONNECTION_UNHEALTHY', 'CONNECTION_CREATED', 'CONNECTION_UPDATED', 'CONNECTION_DELETED', 'CONNECTION_PAUSED', 'CONNECTION_UNPAUSED', 'INTEGRATION_ACTIVATED', 'INTEGRATION_DEACTIVATED', 'INTEGRATION_UPDATED', 'WORKSPACE_UPDATED', 'WORKSPACE_OVER_LIMIT', 'WORKSPACE_80PERCENT_LIMIT', 'WEBHOOK_CREATED', 'WEBHOOK_DELETED', 'WEBHOOK_UNHEALTHY', 'WEBHOOK_PAUSED', 'WEBHOOK_UNPAUSED' ] },
 	custom_auth_domain: { type: String },
 	custom_auth_domain_valid: { type: Boolean },
 	custom_api_domain: { type: String },
+	slow_fields_opt_in: { type: Boolean },
+	notion_verification_token: { type: String },
 };
 
