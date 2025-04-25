9233f395c7d288fa442434e67d7a6f4bceda2db6
Roy Pereira <roymap@users.noreply.github.com>
2025-04-25 15:58:39 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -538,6 +538,7 @@ export interface IIntegrationSupport {
 	list_start_gte?: TSupportInboundType;
 	list_end_le?: TSupportInboundType;
 	list_lead_id?: TSupportInboundType;
+	list_call_id?: TSupportInboundType;
 }
 
 export interface IInvoice {
