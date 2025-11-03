ad44e2ef58eb6f54c0ade47935b25fb83b1be41f
Roy Pereira <roymap@users.noreply.github.com>
2025-11-03 10:38:12 -0500

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -637,6 +637,7 @@ export interface IIntegrationSupport {
 	virtual_webhook_application_id?: TSupportInboundType;
 	native_webhook_event_id?: TSupportInboundType;
 	virtual_webhook_item_variant_id?: TSupportInboundType;
+	virtual_webhook_calendar_id?: TSupportInboundType;
 }
 
 export interface IInvoice {
