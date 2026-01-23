d1c18d0577729ad027f9db703b4da9883a7a4245
Roy Pereira <roymap@users.noreply.github.com>
2026-01-23 17:42:13 -0500

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -521,9 +521,9 @@ export interface IConnection {
 	auth_aws_arn?: string; // the AWS ARN / secretID for the stored auth field
 	environment?: string;
 	auth_azure_keyvault_id?: string; // the Azure Key Vault ID for the stored auth field
+	auth_gcp_secret_name?: string; // the Google Cloud Secret Manager name for the stored auth field
 	last_healthy_at?: (string | Date | number);
 	last_unhealthy_at?: (string | Date | number);
-	auth_gcp_secret_name?: string; // the Google Cloud Secret Manager name for the stored auth field
 	auth_hashi_vault_path?: string; // the HashiCorp Vault path for the stored auth field
 }
 
