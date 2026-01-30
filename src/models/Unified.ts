7472c87ded11ea0d43704cf187af09b02257f4f7
Roy Pereira <roymap@users.noreply.github.com>
2026-01-30 07:52:10 -0500

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -903,6 +903,7 @@ export interface IWorkspace {
 	hashicorp_vault_kv_mount?: string;
 	hashicorp_vault_kv_version?: string; // 1 or 2
 	grafana_apikey?: string;
+	grafana_site?: string;
 }
 
 export interface IWorkspaceIntegration {
