bd30a80237ff89a92e869557568c43977e8b1e05
Roy Pereira <roymap@users.noreply.github.com>
2026-02-13 16:07:21 -0500

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -969,6 +969,7 @@ export interface IWorkspaceIntegration {
 export const CATEGORIES: ICategories[] = [
     { category: 'crm', label: 'CRM' },
     { category: 'martech', label: 'Marketing' },
-    { category: 'uc', label: 'Unified Communications' }
+    { category: 'uc', label: 'Unified Communications' },
+    { category: 'enrich', label: 'Enrichment' }
 ];
 
