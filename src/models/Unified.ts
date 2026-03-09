b25943b2e996a36801d1947b927a3b8ed9f33840
Roy Pereira <roymap@users.noreply.github.com>
2026-03-09 13:49:22 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -772,7 +772,7 @@ export interface IIntegrationSupport {
 	virtual_webhook_bill_id?: TSupportInboundType;
 	virtual_webhook_invoice_id?: TSupportInboundType;
 	list_bill_id?: TSupportInboundType;
-	virtual_webhook_benefit_id?: string;
+	virtual_webhook_benefit_id?: IIntegrationSupport;
 	virtual_webhook_payment_id?: IIntegrationSupport;
 }
 
