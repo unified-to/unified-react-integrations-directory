115b87a86115411efe1b640e7f416f2b1827e509
Roy Pereira <roymap@users.noreply.github.com>
2026-03-09 14:03:47 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -587,6 +587,7 @@ export interface IConnectionAuth {
 	refresh_token_expires_in?: number;
 	refresh_token_expires_date?: (string | Date | number);
 	dev_api_key?: string;
+	audience?: string;
 }
 
 export interface IIntegration {
