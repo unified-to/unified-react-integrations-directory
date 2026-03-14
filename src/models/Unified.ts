81ece6867109fb03e5782796363d8e48f5de8c64
Roy Pereira <roymap@users.noreply.github.com>
2026-03-14 19:18:49 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -274,6 +274,7 @@ export const IntegrationPermission = [
 	'ads_insertionorder_read',
 	'ads_insertionorder_write',
 	'ads_target_read',
+	'ads_promoted_read',
 	'forms_form_read',
 	'forms_form_write',
 	'forms_submission_read',
