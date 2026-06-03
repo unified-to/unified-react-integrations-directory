159ff0f7c037a9011f909ed4d7ed6b9e949fe4db
Roy Pereira <roymap@users.noreply.github.com>
2026-06-03 16:13:26 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -1081,6 +1081,18 @@ export interface IWorkspaceIpAllowlistEntry {
 	expires_at?: (string | Date | number);
 }
 
+export interface ISecretsManager {
+	id?: string;
+	created_at?: (string | Date | number);
+	updated_at?: (string | Date | number);
+	type: TSecretsManagerType;
+	name: string;
+	workspace_id?: string;
+	auth: IRecord<string, string>;
+	environments?: string[];
+	dcs?: string[];
+}
+
 export const CATEGORIES: ICategories[] = [
     { category: 'crm', label: 'CRM' },
     { category: 'martech', label: 'Marketing' },
