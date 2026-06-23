803b20e1f40a9e0b61e5dea24cebbbfb0b6ee1cb
Roy Pereira <roymap@users.noreply.github.com>
2026-06-23 12:50:00 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -1020,7 +1020,6 @@ export interface IWorkspace {
 	secret?: string; // Workspace API secret
 	ip_allowlist?: IWorkspaceIpAllowlistEntry[];
 	restrict_ips?: boolean;
-	auth_aws_arn?: string; // ARN will be used to assume the role for the workspace's AWS resources. (i.e. secret manager)
 	datadog_apikey?: string;
 	datadog_site?: string;
 	environments?: string[]; // a list of authentication environments for the workspace integrations
