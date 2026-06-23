977fdb742212d0bfceb6805fac32d40e5726bcac
Roy Pereira <roymap@users.noreply.github.com>
2026-06-23 12:49:45 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -1020,7 +1020,6 @@ export interface IWorkspace {
 	secret?: string; // Workspace API secret
 	ip_allowlist?: IWorkspaceIpAllowlistEntry[];
 	restrict_ips?: boolean;
-	aws_key?: string;
 	aws_secret?: string;
 	auth_aws_external_id?: string; // External ID will be the identifier used by the customer to verify the role assumption.
 	auth_aws_arn?: string; // ARN will be used to assume the role for the workspace's AWS resources. (i.e. secret manager)
