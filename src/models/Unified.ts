87a310ff0e6df81f1317d1c59fb521412360cd2a
Roy Pereira <roymap@users.noreply.github.com>
2026-07-24 14:03:40 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -1162,6 +1162,7 @@ export interface IWorkspaceIntegrationAuth {
 	pem?: string; // the PEM X.509 certificate in Base64 ASCII format
 	key?: string; // the private KEY X.509 certificate in Base64 ASCII format
 	dev_api_key?: string;
+	saml_idp_url?: string; // the SAML IdP SSO/login URL for end-user SAML login
 }
 
 export interface IWorkspaceIpAllowlistEntry {
