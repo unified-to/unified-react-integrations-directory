90c8f77ea428385878026ae81c73837b12e4b3e8
Roy Pereira <roymap@users.noreply.github.com>
2026-08-10 11:12:33 -0400

    Unified.ts

--- src/models/Unified.ts
+++ src/models/Unified.ts
@@ -1235,6 +1235,7 @@ export const CATEGORIES: ICategories[] = [
     { category: 'datastore', label: 'Datastore' },
     { category: 'analytics', label: 'Analytics' },
     { category: 'cdp', label: 'Customer Data Platform' },
-    { category: 'saml', label: 'SAML SSO Authentication' }
+    { category: 'saml', label: 'SAML SSO Authentication' },
+    { category: 'performance', label: 'Performance Management' }
 ];
 
